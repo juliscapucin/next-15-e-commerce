895a7c4193448118a52d053db73b31518c01845d
juliscapucin <jscapucin@gmail.com>
2024-11-07 18:04:41 +0100

    pages: fix layout, add project page links to content

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -7,6 +7,12 @@ export const structure: StructureResolver = (S) =>
 		.items([
 			S.documentTypeListItem("category").title("Categories"),
 			S.divider(),
+			S.listItem().title("Sections").child(
+				S.list().title("Sections").items([
+					// Add your section items here
+				])
+			),
+			S.divider(),
 			...S.documentTypeListItems().filter(
 				(item) => item.getId() && !["category"].includes(item.getId()!)
 			),
